e56db15ee7b77e82cb5b32b979731d260f8d7aeb
Elena Sergienko <elenasergienko@Elenas-MacBook-Pro-2.local>
2023-02-02 13:33:15 -0800

    refactoring: feature-sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
 import React, {Suspense, useContext, useState} from 'react';
 import {Route, Routes, Link} from 'react-router-dom';
 import './styles/index.scss';
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {useTheme} from "./styles/theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {MainPage} from "pages/MainPage";
+import {AboutPage} from "pages/AboutPage";
 
 const App = () => {
 const { theme, toggleTheme } = useTheme();
-const bool = true;
 
     return (
         <div className={classNames('app', {hovered: true, selected: false}, [theme])}>
@@ -18,8 +17,8 @@ const bool = true;
 
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
                 </Routes>
             </Suspense>
 
