72e9d46d1edcbdd5e2f2a08d3c571f9b60479e41
Elena Sergienko <elenasergienko@Elenas-MacBook-Pro-2.local>
2023-03-07 23:40:56 -0800

    add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import AppRouter from "./providers/router/ui/AppRouter";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
 
-// lesson 14 completed
+const Component = () => {
+    const { t, i18n } = useTranslation();
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en')
+    }
+
+    return (
+        <div>
+            <button onClick={toggle}>{t('Translate')}</button>
+            {t('Test example')}
+        </div>
+    )
+}
+
+// lesson 15 - 11:20
 
 const App = () => {
     const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {hovered: true, selected: false}, [theme])}>
+            <Suspense fallback="">
             <Navbar/>
+                <Component/>
             <div className="content-page">
                 <Sidebar/>
                 <AppRouter/>
             </div>
+            </Suspense>
         </div>
     );
 };
