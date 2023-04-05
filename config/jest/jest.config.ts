8edd8a35088e061e75abfcadb22975e72bccdfe4
Elena-Sergienko <elena.ser.us@gmail.com>
2023-04-04 23:40:43 -0700

    add tests for btn, sidebar; upd config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
     // All imported modules in your tests should be mocked automatically
     // automock: false,
@@ -21,6 +23,7 @@ export default {
     ],
     moduleDirectories: [
         'node_modules',
+        'src',
     ],
     modulePaths: [
         '<rootDir>src',
@@ -37,6 +40,7 @@ export default {
     setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
     testMatch: [
         '**/__tests__/**/*.[jt]s?(x)',
