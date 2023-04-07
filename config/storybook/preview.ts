876808afef5eb7d74a1448ecd89d7954f32b1c0c
Elena Sergienko <elenasergienko@Elenas-MacBook-Pro-2.local>
2023-04-06 23:23:03 -0700

    add stories

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/react';
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 
 const preview: Preview = {
     parameters: {
@@ -17,4 +18,4 @@ const preview: Preview = {
 
 export default preview;
 
-export const decorators = [StyleDecorator, ThemeDecorator(Theme.LIGHT)];
+export const decorators = [StyleDecorator, ThemeDecorator(Theme.LIGHT), RouterDecorator];
